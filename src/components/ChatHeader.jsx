08fbd0e241c7b042732f86195a4290a4903f4cda
LeTrungDuy <letrungduy11496@gmail.com>
2026-03-19 16:23:36 +0700

    feat: add logo template in header

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,7 +1,9 @@
+import templateIcon from '../assets/template-icon.svg';
+
 const ChatHeader = () => {
   return (
     <div className="py-6 border-b border-gray-200 ml-[50px] md:ml-0">
-      <h1 className="text-2xl font-bold text-gray-900">Hi! Template</h1>
+      <img src={templateIcon} alt="Gemini" className="w-[134px] h-[22px]" />
     </div>
   );
 };
